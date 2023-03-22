3dde3c69b303c99fd09346a4fd77411d18b7691c
paradox <get_ant@naver.com>
2023-03-22 17:01:10 +0900

    초기 커밋.

---
+++ Algorithm_C/01_Basic_Recursion.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int Fact(int n) {
+    if (n == 1 || n == 0) return 1;
+    return n * Fact(n - 1);
+}
+int Fibo(int n) {
+    if (n == 0 || n == 1) return n;
+    return Fibo(n - 1) + Fibo(n - 2);
+}
+
+int main()
+{
+    cout << Fact(5) << '\n';
+    cout << Fibo(5) << '\n';
+
+    return 0;
+}
---
+++ Algorithm_C/02_Next_Permutation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //순열
+    // nPr = n! / (n-r)!
+    // n개중 r을 뽑음
+   
+
+    cout << "hello world";
+
+    return 0;
+}
